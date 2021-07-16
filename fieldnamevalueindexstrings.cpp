2dc1070d100096eb36968346c3c7e4f3c92ae248
Andrei Ivanov <andrei.i.ivanov@commandus.com>
2021-07-16 14:46:52 +0900

    fix alias db

--- fieldnamevalueindexstrings.cpp
+++ fieldnamevalueindexstrings.cpp
@@ -10,7 +10,7 @@
 
 #include "utilstring.h"
 
-#define PROPERTY_INT_TIME "time"
+#define PROPERTY_INT_TIME	"time"
 
 static const std::string sql2tableNumeric = "num";
 static const std::string sql2tableString = "str";
@@ -191,10 +191,12 @@ void FieldNameValueIndexStrings::toStringInsert
 		{
 			if (fieldCount)
 				ss << ", ";
+			/*	
 			if ((it->first == PROPERTY_INT_TIME) && pkt2utilstring::isNumber(it->second))
 				ss << it->second;
 			else
-				ss << string_quote << it->second << string_quote;
+			*/
+			ss << string_quote << it->second << string_quote;
 			fieldCount++;
 		}
 	}
@@ -568,10 +570,12 @@ std::string FieldNameValueIndexStrings::toStringJSON(
 				ss << ", ";
 			addColon = true;
 			ss << "\"" << it->first << "\": ";
+			/*
 			if ((it->first == PROPERTY_INT_TIME) && pkt2utilstring::isNumber(it->second))
 				ss << it->second;
 			else
-				ss << "\"" << it->second << "\"";
+			*/
+			ss << "\"" << it->second << "\"";
 		}
 	}
 
